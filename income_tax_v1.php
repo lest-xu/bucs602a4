29493123bd1e6428c50a782459e84121cd753960
“lest”-xu <autodataxx@gmail.com>
2024-07-24 16:51:47 -0700

    update

--- income_tax_v1.php
+++ income_tax_v1.php
@@ -5,10 +5,30 @@
 
 function incomeTaxSingle($taxableIncome) {
     $incTax = 0.0;
+    if ($taxableIncome > 0 && $taxableIncome <= 9700) {
+        # 10% of income
+        return $incTax = $taxableIncome * 0.1;
+    } else if ($taxableIncome > 9700 && $taxableIncome <= 39475) {
+        # 12% of income + 970
+        return $incTax = $taxableIncome * 0.12 + 970;
+    } else if ($taxableIncome > 39475 && $taxableIncome <= 84200) {
+        # 22% of income + 4543
+        return $incTax = $taxableIncome * 0.22 + 4543;
+    } else if ($taxableIncome > 84200 && $taxableIncome <= 160725) {
+        # 24% of income + 14382
+        return $incTax = $taxableIncome * 0.24 + 14382;
+    } else if ($taxableIncome > 160725 && $taxableIncome <= 204100) {
+        # 32% of income + 32748
+        return $incTax = $taxableIncome * 0.32 + 32748;
+    } else if ($taxableIncome > 204100 && $taxableIncome <= 510300) {
+        # 35% of income + 46628
+        return $incTax = $taxableIncome * 0.35 + 46628;
+    }  else if ($taxableIncome > 510300) {
+        # 37% of income + 46628
+        return $incTax = $taxableIncome * 0.37 + 153798;
+    } 
 
-    
     return $incTax;
-
 }
 
 function incomeTaxMarriedJointly($taxableIncome) {
