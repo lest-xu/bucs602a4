8d8e3b8ee02f0e90852e379624b9959fd7586cda
“lest”-xu <autodataxx@gmail.com>
2024-07-25 11:57:33 -0700

    update

--- income_tax_v2.php
+++ income_tax_v2.php
@@ -80,7 +80,7 @@ function incomeTax($taxableIncome, $status) {
 
             echo "Results...";
 
-            echo $TAX_RATES['Single']['Ranges'];
+            echo TAX_RATES['Single']['Ranges'];
 
 
         }
